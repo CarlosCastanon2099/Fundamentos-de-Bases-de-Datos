745e8a2a42ba36dfa9302e42e59720ebed0ac5a1
Cristian Gallegos <72717970+CristianRicardoGallegosDiego@users.noreply.github.com>
2023-11-09 20:30:13 -0600

    CorreoClienteRowMapper terminado

--- Practicas/Practica08/src/main/java/practica08FBDD/Mapper/CorreoClienteRowMapper.java
+++ Practicas/Practica08/src/main/java/practica08FBDD/Mapper/CorreoClienteRowMapper.java
@@ -10,11 +10,21 @@ import org.springframework.jdbc.core.RowMapper;
 import practica08FBDD.model.CorreoCliente;
 
 /**
- *
- * @author crgal
+ * La clase CorreoClienteRowMapper implementa la interfaz RowMapper de Spring, que se utiliza para mapear filas de un ResultSet a objetos de tipo CorreoCliente.
+ * Cada instancia de esta clase representa la lógica de mapeo de una fila del conjunto de resultados a un objeto CorreoCliente.
+ * Esta clase se utiliza generalmente en combinación con JdbcTemplate de Spring para realizar operaciones de mapeo de datos.
+ * @author Cristian Gallegos
+ * @version 8 - Noviembre - 2023
  */
 public class CorreoClienteRowMapper implements RowMapper<CorreoCliente> {
     
+    /**
+     * Mapea una fila del ResultSet a un objeto CorreoCliente.
+     * @param rs ResultSet que contiene los datos de la fila.
+     * @param rowNum Número de la fila actual.
+     * @return Objeto CorreoCliente creado a partir de los datos de la fila.
+     * @throws SQLException Si hay algún error al acceder a los datos del ResultSet.
+     */
     @Override
     public CorreoCliente mapRow(ResultSet rs, int rowNum) throws SQLException {
         CorreoCliente cc = new CorreoCliente(
