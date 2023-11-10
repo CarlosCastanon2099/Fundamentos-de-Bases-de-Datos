ba9192104156c7ff5e2e441b05807c873e3d7b22
Cristian Gallegos <72717970+CristianRicardoGallegosDiego@users.noreply.github.com>
2023-11-09 20:38:22 -0600

    CorreoClienteServicioImp clase terminada con documentacion

--- Practicas/Practica08/src/main/java/practica08FBDD/Service/CorreoClienteServicioImp.java
+++ Practicas/Practica08/src/main/java/practica08FBDD/Service/CorreoClienteServicioImp.java
@@ -11,35 +11,57 @@ import practica08FBDD.Repository.CorreoClienteRepositorio;
 import practica08FBDD.model.CorreoCliente;
 
 /**
- *
+ * Implementación de servicio que proporciona operaciones de negocio relacionadas con la entidad CorreoCliente.
+ * Esta clase actúa como una capa intermedia entre la capa de controladores y la capa de acceso a datos,
+ * utilizando la interfaz CorreoClienteRepositorio para realizar operaciones CRUD en la base de datos.
  * @author crgal
  */
 @Service
 public class CorreoClienteServicioImp implements CorreoClienteServicio {
     
     @Resource
-    CorreoClienteRepositorio correoClienteRep;
+    CorreoClienteRepositorio correoClienteRep; // Repositorio que maneja las operaciones de acceso a datos para la entidad CorreoCliente.
 
+    /**
+     * Recupera todos los registros de CorreoCliente desde la base de datos.
+     * @return Lista de objetos CorreoCliente.
+     */
     @Override
     public List<CorreoCliente> findAll() {
         return correoClienteRep.findAll();
     }
 
+    /**
+     * Inserta un nuevo registro de CorreoCliente en la base de datos.
+     * @param cc Objeto CorreoCliente a ser insertado.
+     */
     @Override
     public void insertCorreoCliente(CorreoCliente cc) {
         correoClienteRep.insertCorreoCliente(cc);
     }
 
+    /**
+     * Actualiza un registro existente de CorreoCliente en la base de datos.
+     * @param cc Objeto CorreoCliente con los datos actualizados.
+     */
     @Override
     public void updateCorreoCliente(CorreoCliente cc) {
         correoClienteRep.updateCorreoCliente(cc);
     }
 
+    /**
+     * Método que actualiza el CorreoCliente en el servicio web
+     * @param cc El CorreoCliente que se actualiza en el servicio web.
+     */
     @Override
     public void executeUpdateCorreoCliente(CorreoCliente cc) {
         correoClienteRep.executeUpdateCorreoCliente(cc);
     }
 
+    /**
+     * Elimina un registro de CorreoCliente de la base de datos.
+     * @param cc Objeto CorreoCliente a ser eliminado.
+     */
     @Override
     public void deleteCorreoCliente(CorreoCliente cc) {
         correoClienteRep.deleteCorreoCliente(cc);
