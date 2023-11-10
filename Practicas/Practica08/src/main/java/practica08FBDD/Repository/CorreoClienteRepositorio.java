95263021c86847bfadacbdb5102a5ca3d3a52657
Cristian Gallegos <72717970+CristianRicardoGallegosDiego@users.noreply.github.com>
2023-11-09 20:33:38 -0600

    CorreoClienteRepositorio terminado

--- Practicas/Practica08/src/main/java/practica08FBDD/Repository/CorreoClienteRepositorio.java
+++ Practicas/Practica08/src/main/java/practica08FBDD/Repository/CorreoClienteRepositorio.java
@@ -9,19 +9,42 @@ import practica08FBDD.model.CorreoCliente;
  */
 
 /**
- *
+ * Interfaz que define las operaciones de acceso a datos (CRUD) para la entidad CorreoCliente.
+ * Las clases que implementan esta interfaz proporcionan métodos para realizar operaciones de lectura
+ * y escritura en la base de datos relacionadas con la entidad CorreoCliente.
  * @author crgal
  */
 public interface CorreoClienteRepositorio {
     
-    List<CorreoCliente> findAll();
+    /**
+     * Recupera todos los registros de CorreoCliente desde la base de datos.
+     * @return Lista de objetos CorreoCliente.
+     */
+    public List<CorreoCliente> findAll();
     
+    /**
+     * Inserta un nuevo registro de CorreoCliente en la base de datos.
+     * @param cc Objeto CorreoCliente a ser insertado.
+     */
     public void insertCorreoCliente(CorreoCliente cc);
     
+    /**
+     * Actualiza un registro existente de CorreoCliente en la base de datos.
+     * @param cc Objeto CorreoCliente con los datos actualizados.
+     */
     public void updateCorreoCliente(CorreoCliente cc);
     
+    /**
+     * Método que dado un Cliente, se encarga de ejecutar la acutalización
+     * en la base de datos creando la petición a través de los datos de 
+     * la instancia CorreoCliente, usando SQL.
+     * @param cc - - El cliente a actualizar
+     */
     public void executeUpdateCorreoCliente(CorreoCliente cc);
     
+    /**
+     * Elimina un registro de CorreoCliente de la base de datos.
+     * @param cc Objeto CorreoCliente a ser eliminado.
+     */
     public void deleteCorreoCliente(CorreoCliente cc);
-    
 }
